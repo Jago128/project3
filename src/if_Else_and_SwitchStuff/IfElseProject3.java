a16ec20fa83e379535014a5038be749b46aa4920
Jago128 <jagobaetxegarai@gmail.com>
2024-12-12 13:07:12 +0100

    If, Else, and Switch stuff

---
+++ src/if_Else_and_SwitchStuff/IfElseProject3.java
@@ -0,0 +1,20 @@
+package if_Else_and_SwitchStuff;
+import java.util.Scanner;
+public class IfElseProject3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		double num, cuadrado;
+		
+		System.out.print("Intruduce el numero: ");
+		num = teclado.nextDouble();
+		cuadrado=num*num;
+		if (cuadrado>100) {
+			System.out.println(cuadrado-100);
+		} else  {System.out.println("Falta esto para llegar a 100.");
+			System.out.println(100-cuadrado);
+		}
+		teclado.close();
+	}
+}
